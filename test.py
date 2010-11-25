b86dd95c3dd7ff41ae91ba90645e904672c6ad50
Oneiroi <d.busby@saiweb.co.uk>
2010-11-25 17:15:07 +0000

    Working version

--- test.py
+++ test.py
@@ -4,7 +4,5 @@ if len(sys.argv) < 1:
 	print 'usage:'
 	print sys.argv[0],' /path/to/combined.log'
 	sys.exit(1)
-cdll.LoadLibrary('build/lib.macosx-10.6-universal-2.6/apache_combined_parser.so')
-from build/lib.macosx-10.6-universal-2.6/apache_combined_parser import *
-#acp.parse_by_hour(sys.argv[1])
-print dir(acp)
+from apache_combined_parser import *
+parse_by_hour(sys.argv[1])
