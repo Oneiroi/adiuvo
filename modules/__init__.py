586a5f02dfd98932ffaaae73f70d354208e937b9
Oneiroi <d.busby@saiweb.co.uk>
2011-06-13 11:12:51 +0100

    Removing legacy files see python_deprecated branch for legacy files

--- modules/__init__.py
+++
@@ -1,7 +0,0 @@
-'''
-__author__="David Busby"
-__copyright__="David Busby <d.busby@saiweb.co.uk>, Psycle Interactive Limited <david.busby@psycle.com>"
-__license__="GNU v3 + part 5d section 7: Redistribution/Reuse of this code is permitted under the GNU v3 license, as an additional term ALL code must carry the original Author(s) credit in comment form."
-'''
-from cli_progress import *
-from apache_combined_parser import *
--- test.py
+++
@@ -1,8 +0,0 @@
-import sys
-from ctypes import *
-if len(sys.argv) < 1:
-	print 'usage:'
-	print sys.argv[0],' /path/to/combined.log'
-	sys.exit(1)
-from apache_combined_parser import *
-parse_by_hour(sys.argv[1])
